88c7732683b575fa98fa8583ec253e8a0bb2987d
arnabjyoti <thakuriaarnabjyoti@gmail.com>
2024-07-23 11:25:26 +0530

    table

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Login from './AdminPages/form/login';
 import Dashboard from './AdminPages/dashboard/dashboard';
 import DashboardPage from './AdminPages/pages/DashboardPage';
 
-import AddJournal from './AdminPages/pages/addJournal';
+import NewRequest from './AdminPages/pages/newRequest';
 import AdminCategory from './AdminPages/pages/category';
 import AdminPublisher from './AdminPages/pages/publisher';
 import JournalTable from './AdminPages/table/journalTable';
@@ -66,7 +66,7 @@ function App() {
           {/* admin  */}
           <Route path='/admin' element={<Dashboard />}>
             <Route path='/admin' element={<DashboardPage />} />
-            <Route path='addJournal' element={<AddJournal />} />
+            <Route path='newRequest' element={<NewRequest />} />
             <Route path='category' element={<AdminCategory />} />
             <Route path='publisher' element={<AdminPublisher />} />
             <Route path='JournalTable' element={<JournalTable />} />
