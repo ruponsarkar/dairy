8880b26d8f78ab9f00e28b20b03c7c34f2b0eb54
Samaruzzaman <https://skaplink_projects@bitbucket.org/gmcproject/gmchrms_production.git>
2024-08-22 11:34:57 +0530

    Merging Latest Code

--- src/API/apiConfig.js
+++ src/API/apiConfig.js
@@ -5,8 +5,8 @@ import AuthUser from './token';
 // const token = AuthUser();
 const token = JSON.parse(sessionStorage.getItem('token'));
 const API = axios.create({
-    // baseURL : "https://milksubsidydairyassam.com:8800/",
-    baseURL : "http://127.0.0.1:8800/",
+    baseURL : "https://milksubsidydairyassam.com:8800/",
+    // baseURL : "http://127.0.0.1:8800/",
     headers :{
         "Content-Type": "application/json",
         "Authorization" : `Bearer ${token}`
